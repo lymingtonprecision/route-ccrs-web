5c8225e15930a2fdd666f571973a105fce871dbd
Adam Harper <github@adam-harper.com>
2016-05-19 11:55:29 +0100

    Update to `route-ccrs` version 3.4.0 (and bump other dependencies)

--- project.clj
+++ project.clj
@@ -3,33 +3,33 @@
   :url "https://github.com/lymingtonprecision/route-ccrs-web"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
 
                  ; components/system
-                 [com.stuartsierra/component "0.2.3"]
-                 [environ "1.0.0"]
-                 [prismatic/plumbing "0.4.2"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [environ "1.0.3"]
+                 [prismatic/plumbing "0.5.3"]
 
                  ; logging
                  [org.clojure/tools.logging "0.3.1"]
                  [org.spootnik/logconfig "0.7.3"]
 
                  ; database
-                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.clojure/java.jdbc "0.6.1"]
                  [org.clojars.zentrope/ojdbc "11.2.0.3.0"]
-                 [hikari-cp "1.2.2"]
+                 [hikari-cp "1.6.1"]
 
                  ; web
                  [http-kit "2.1.19"]
-                 [ring/ring-core "1.3.2"
+                 [ring/ring-core "1.4.0"
                   :exclusions [clj-time]]
-                 [prismatic/fnhouse "0.1.1"]
-                 [com.cognitect/transit-clj "0.8.271"]
-                 [ring-transit "0.1.3"]
+                 [prismatic/fnhouse "0.2.1"]
+                 [com.cognitect/transit-clj "0.8.285"]
+                 [ring-transit "0.1.4"]
 
                  ; data processing
-                 [prismatic/schema "0.4.2"]
-                 [lymingtonprecision/route-ccrs "3.2.0"]]
+                 [prismatic/schema "1.1.1"]
+                 [lymingtonprecision/route-ccrs "3.4.0"]]
 
   :main route-ccrs-web.main
 
